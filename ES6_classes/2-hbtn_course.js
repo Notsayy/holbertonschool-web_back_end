bf255c749211f12e15f6f3a9f42f1f19294fcd06
Notsayy <jtertre25@gmail.com>
2025-03-29 11:26:19 +0100

    Fix error message

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,10 +1,10 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw new TypeError('Name should be a string');
+      throw new TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
-      throw new TypeError('Length should be a number');
+      throw new TypeError('Length must be a number');
     }
     if (
       !Array.isArray(students)
@@ -23,7 +23,7 @@ export default class HolbertonCourse {
 
   set name(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('Name should be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
@@ -34,7 +34,7 @@ export default class HolbertonCourse {
 
   set length(value) {
     if (typeof value !== 'number') {
-      throw new TypeError('Length should be a number');
+      throw new TypeError('Length must be a number');
     }
     this._length = value;
   }
