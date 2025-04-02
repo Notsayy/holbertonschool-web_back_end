babf52d446dd0ab77679dc45e9e6b53ea9bee563
Notsayy <jtertre25@gmail.com>
2025-04-02 16:27:18 +0200

    Task 9:  Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardail was processed');
+  }
+  return queue;
+}
