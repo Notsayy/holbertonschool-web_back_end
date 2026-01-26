460f0c8d0b2db3b38b6c1054db6d5fcb31450743
Notsayy <jtertre25@gmail.com>
2026-01-26 10:20:56 +0100

    Task 1: FIFO caching

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" FIFOCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache class"""
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data:
+            self.order.append(key)
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key = self.order.pop(0)
+            del self.cache_data[first_key]
+            print("DISCARD: {}".format(first_key))
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
