06a2ac75fee2417bb41c947d9ab6f0e7d769f564
Notsayy <jtertre25@gmail.com>
2026-04-21 11:51:54 +0200

    Task 7

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 """Unit tests for client module."""
+import unittest
 from parameterized import parameterized
 from client import GithubOrgClient
 
 
-@parameterized.expand([
-    ({"license": {"key": "my_license"}}, "my_license", True),
-    ({"license": {"key": "other_license"}}, "my_license", False),
-])
-def test_has_license(
-    self,
-    repo: dict,
-    license_key: str,
-    expected: bool
-) -> None:
-    """Unit-test GithubOrgClient.has_license"""
-    self.assertEqual(
-        GithubOrgClient.has_license(repo, license_key),
-        expected
-    )
+class TestGithubOrgClient(unittest.TestCase):
+    """Test cases for GithubOrgClient"""
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(
+        self,
+        repo: dict,
+        license_key: str,
+        expected: bool
+    ) -> None:
+        """Unit-test GithubOrgClient.has_license"""
+        self.assertEqual(
+            GithubOrgClient.has_license(repo, license_key),
+            expected
+        )
