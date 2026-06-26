54d659b194e615f09dbbdc552997bb79442c4b80
Notsayy <jtertre25@gmail.com>
2026-06-26 09:22:57 +0200

    Add comments

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,4 @@
+-- Task 0: Create users table with unique email constraint
 CREATE TABLE IF NOT EXISTS users (
     id    INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
